6222ce7289a53e69052210bd7ce777159544445c
svs <svs@localhost>
2025-08-12 05:13:09 +0300

    zip command, date in the filename, arguments in scripts, no window under windows

--- freemind/start-freemind.sh
+++ freemind/start-freemind.sh
@@ -10,4 +10,4 @@ dir_path=$(dirname $full_path)
 
 export JAVA_HOME=$dir_path/jdk-11.0.28+6-jre
 PATH=$JAVA_HOME/bin:$PATH
-$dir_path/freemind-bin-max-1.0.1/freemind.sh
+$dir_path/freemind-bin-max-1.0.1/freemind.sh "$@"
